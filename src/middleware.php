5d01c6c55ea1e0b4058e56644b4d2f67ab939e9c
José Felipe da Silva <jfsfelipe2011@gmail.com>
2018-10-13 14:40:16 -0300

    Ajuste para o CORS da interface

--- src/middleware.php
+++ src/middleware.php
@@ -2,6 +2,15 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+// 
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
 
 $app->add(new \Tuupola\Middleware\HttpBasicAuthentication([
 
